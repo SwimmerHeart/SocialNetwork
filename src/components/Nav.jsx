635001f3443478cf1c64793844e51b8edfa8cb48
SwimmerHeart <62188802+SwimmerHeart@users.noreply.github.com>
2020-03-15 00:07:38 +0300

    init commit 1

---
+++ src/components/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <ul>
+                <li>
+                    <a>Profile</a>
+                </li>
+                <li>
+                    <a>Messages</a>
+                </li>
+                <li>
+                    <a>News</a>
+                </li>
+                <li>
+                    <a>Misic</a>
+                </li>
+                <li>
+                    <a>Settings</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://startupanz.com/wp-content/uploads/2019/07/maxresdefault-1.jpg" />
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My product
+            </div>
+            <div>
+                New post
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
